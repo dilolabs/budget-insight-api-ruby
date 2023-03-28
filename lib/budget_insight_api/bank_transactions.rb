db3171255cc00534f1ee09922efd62fde345b9c3
Dilolabs <contact@dilolabs.fr>
2023-03-28 11:30:42 +0200

    Add cursor to transactions

--- lib/budget_insight_api/bank_transactions.rb
+++ lib/budget_insight_api/bank_transactions.rb
@@ -2,9 +2,11 @@
 
 module BudgetInsightAPI
   class BankTransactions
-    def self.list(client, user_token, user_id, account_id, limit)
-      client.connection.get("/2.0/users/#{user_id}/accounts/#{account_id}/transactions?limit=#{limit}") do |request|
+    def self.list(client, user_token, user_id, account_id, limit, cursor = nil)
+      client.connection.get("/2.0/users/#{user_id}/accounts/#{account_id}/transactions") do |request|
         request.headers[:authorization] = "Bearer #{user_token}"
+        request.params["limit"] = limit
+        request.params["cursor"] = cursor if cursor.present?
       end
     end
   end
